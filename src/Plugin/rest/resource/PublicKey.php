76aa37f1879aadf3188964d405f4aaa2f3e97a64
tameeshb <tameeshbiswas1998@gmail.com>
2017-06-16 00:02:48 +0530

    REST resource for /publicKey handling both POST and GET

---
+++ src/Plugin/rest/resource/PublicKey.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Drupal\client_side_file_crypto\Plugin\rest\resource;
+
+use Drupal\user\Entity\User;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "public_key",
+ *   label = @Translation("Public key"),
+ *   uri_paths = {
+ *     "canonical" = "//publicKey",
+ *     "https://www.drupal.org/link-relations/create" = "/publicKey"
+ *   }
+ * )
+ */
+class PublicKey extends ResourceBase {
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new PublicKey object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('client_side_file_crypto'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * Returns a list of bundles for specified entity.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws exception expected.
+   */
+  public function get() {
+
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+    $uid = \Drupal::currentUser()->id();
+    if($uid){
+      if ($user = User::load($uid)) {
+        $return["publicKey"] = $user->get('pub_key')->value;
+        $return["uid"] = $user->get('name')->value;
+        $return["status"] = "Success!";
+        $return["sCode"] = 1;
+        return new ResourceResponse($return);
+      }else{
+        //error loading user
+        throw new \Exception();
+      }
+    }else{
+      //user not logged in
+      throw new AccessDeniedHttpException();
+    }
+  }
+
+  /**
+   * Responds to POST requests.
+   *
+   * Returns a list of bundles for specified entity.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws exception expected.
+   */
+  public function post(array $data = []) {
+
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+    if ($user = User::load(\Drupal::currentUser()->id())) {
+      //data validation
+      if ($user->set('pub_key', $data['publicKey'])->save()) {
+        $return["status"] = 1;
+        $return["message"] = "Successfully added.";
+      }
+    }
+    else {
+      $return["status"] = -1;
+      $return["message"] = "Error occured.";
+      throw new AccessDeniedHttpException();
+    }
+    return new ResourceResponse($return);
+  }
+
+}
