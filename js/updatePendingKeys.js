d4f24da098f1514a7ef13a028ac0a04f7fb2c126
tameeshb <tameeshbiswas1998@gmail.com>
2017-07-07 06:04:38 +0530

    accessKey gen fix

--- js/updatePendingKeys.js
+++ js/updatePendingKeys.js
@@ -2,62 +2,88 @@
 (function ($) {
   // Jquery onload function.
   $(document).ready(function(){
-  				var new_accessKey_ = '';
-  				$.get( "../publicKey?_format=json", function(pkey) {
-  					console.log(pkey['publicKey']);
-	  	    		var encrypt = new JSEncrypt();
-	  			  	encrypt.setPublicKey(pkey['publicKey']);
-	  			  	new_accessKey_ = encrypt.encrypt("yippie!");
-	  			  	console.log("ciphertext : " + new_accessKey_ );
-	  			  	decrypt.setPrivateKey(localStorage.getItem("privKey"));
-				  	console.log("clearText:" + decrypt.decrypt(new_accessKey_));
+  				// var new_accessKey_ = '';
+  				// $.get( "../publicKey?_format=json", function(pkey) {
+  				// 	console.log(pkey['publicKey']);
+	  	  //   		var encrypt = new JSEncrypt();
+	  	  //   		var decrypt = new JSEncrypt();
+	  			//   	encrypt.setPublicKey(pkey['publicKey']);
+	  			//   	new_accessKey_ = encrypt.encrypt("yippie!");
+	  			//   	console.log("ciphertext : " + new_accessKey_ );
+	  			//   	decrypt.setPrivateKey(localStorage.getItem("privKey"));
+				  // 	console.log("clearText:"  + decrypt.decrypt(new_accessKey_));
 
-  				});
-	  			  	console.log("ciphertext : " + new_accessKey_ );
+  				// });
+//   				var node = {
+// 	"accessKey":"J8i5a+90n0NDhGhyoMxYFO5RWfuVzMrlP6X6oS407TA5wE1mtJ2p9yvFwfcaih9tJT4r2YBjPVMbJOR4HxrdUAPqmbkxL37hRaGGva7nN5EijejqUznG5akYn7mnQiCI1DZ1C709BD2irRdMeIhWsFCgvB/IBSykAwP/XC01S8k=",
+// 	"roleName":"authenticated",
+// 	"userID":"2"
+// };
+//   				  jQuery.ajax({
+  				
+//     url: '../accessKey/?_format=json',
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/hal+json',
+//       'X-CSRF-Token': 'NYhgRSicnDBVPJNQfYy7ZA8DXIPKNccE8xAn3AHygyA'
+//     },
+//     data: JSON.stringify(node),
+//     success: function (node) {
+//       console.log(node);
+//     }
+//   }).done(function( data ) {
+//       console.log( "Sample of data:", data );
+//   });
+		////-----------
+			$.get( "../rest/session/token", function( csrf_t ){
+				$.get( "../publicKey/?_format=json", function( xhr_pub_key ){
+					// console.log("publicKey:"+xhr_pub_key['publicKey']);
+					var publicKey = xhr_pub_key['publicKey'];
+					var privateKey = localStorage.getItem("privKey");
+					var encrypt = new JSEncrypt();
+					// console.log("priv_key : "+ privateKey);
+					$.get( "../accessKey/pending?_format=json", function( pending_keys_json ){
+						var decrypt = new JSEncrypt();
+						decrypt.setPrivateKey(privateKey);
+						pending_keys = pending_keys_json['accessKeys'];
+						pending_keys.forEach(function(accessKey) {
+							var group_access_key = decrypt.decrypt(accessKey['access_key']);
+							encrypt.setPublicKey(accessKey['pub_key']);
+							var new_access_key = encrypt.encrypt(group_access_key);
+							console.log(group_access_key);
+							console.log(new_access_key);
+							var json_body = {
+								"accessKey" : new_access_key,
+								"roleName" : accessKey['role'],
+								"userID" : accessKey['uid'],
+							};
+							var node = {
+								"accessKey":"J8i5a+90n0NDhGhyoMxYFO5RWfuVzMrlP6X6oS407TA5wE1mtJ2p9yvFwfcaih9tJT4r2YBjPVMbJOR4HxrdUAPqmbkxL37hRaGGva7nN5EijejqUznG5akYn7mnQiCI1DZ1C709BD2irRdMeIhWsFCgvB/IBSykAwP/XC01S8k=",
+								"roleName":"authenticated",
+								"userID":"2"
+							};
+							jQuery.ajax({
+											
+							  url: '../accessKey/?_format=json',
+							  method: 'POST',
+							  headers: {
+							    'Content-Type': 'application/hal+json',
+							    'X-CSRF-Token': csrf_t
+							  },
+							  data: JSON.stringify(json_body),
+							  success: function (node) {
+							    console.log(node);
+							  }
+							}).done(function( data ) {
+							    console.log( "Sample of data:", data );
+							});
+							console.log(json_body);
+							// var group_access_key = decrypt.decrypt(accessKey['access_key']);
+						});
 
-  	//
-  	var decrypt = new JSEncrypt();
-  	var privkey = localStorage.getItem("privKey");
-  	console.log("priveKey:" + privkey);
-  	decrypt.setPrivateKey(privkey);
-  	console.log("clearText:" + decrypt.decrypt(new_accessKey_));
-  	var group_access_key_ = decrypt.decrypt(new_accessKey_);
-  	console.log('reached here!');
-    $.get( "../rest/session/token", function(csrf_t) {
-	    $.get( "../accessKey/pending?_format=json", function( pending_keys_json ) {
-	    	pending_keys = pending_keys_json['accessKeys'];
-	    	pending_keys.forEach(function(accessKey) {
-	    		console.log('access_key :' + accessKey['access_key']);
-	    		var group_access_key = decrypt.decrypt(accessKey['access_key']);
-	    		console.log('pending :' + group_access_key);
-	    		var encrypt = new JSEncrypt();
-			  	encrypt.setPublicKey(accessKey['pub_key']);
-	    		console.log("pub_key :" + accessKey['pub_key']);
-			  	var new_accessKey = encrypt.encrypt(group_access_key);
-			  	console.log(new_accessKey);
-			  	var json_body = {
-			  		"accessKey" : new_accessKey,
-			  		"roleName" : accessKey['role'],
-			  		"userID" : accessKey['uid'],
-			  	};
-			  	console.log(json_body);
-			  	jQuery.ajax({
-			  	  url: '../accessKey/?_format=json',
-			  	  method: 'POST',
-			  	  headers: {
-			  	    'Content-Type': 'application/hal+json',
-			  	    'X-CSRF-Token': csrf_t,
-			  	  },
-			  	  data: JSON.stringify(json_body),
-			  	  success: function (response_data) {
-			  	    console.log(response_data);
-			  	  }
-			  	});
-		    	console.log(accessKey);
-	    	});
-
-	    });
-    });
+					});
+				});
+			});
+  	////-----------
   });
 })(jQuery); 
-
