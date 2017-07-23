3bcf3c898e59cebe4a7c5135bc5976123abfb228
tameeshb <tameeshbiswas1998@gmail.com>
2017-07-23 11:05:02 +0530

    Async File Upload plaintext

--- js/encrypt.js
+++ js/encrypt.js
@@ -17,7 +17,7 @@
   // Jquery onload function.
   var file = null;
   $(document).ready(function(){
-  	$("#edit-cryptofields").change(function(e){
+  	$("#cryptoFields").change(function(e){
   		e.preventDefault();
   		//currently only for first file field in DOM, later add a forEach loop
   		file = e.target.files[0];
@@ -40,12 +40,30 @@
 	    		reader.onload = function(event_){
 	    			var encrypted = CryptoJS.AES.encrypt(event_.target.result, group_access_key); 
 	    			console.log("cipherText: ",encrypted);
-	    			download('encrypted.txt',encrypted);
-	    			// a.attr('href', 'data:application/octet-stream,' + encrypted);
-	    			// a.attr('download', file.name + '.encrypted');
+	    			download(file_name,encrypted);
+	    			var boundary = "---------------------------7da24f2e50046";
+	    			var body = '--' + boundary + '\r\n'
+	    			         // Parameter name is "file" and local filename is "temp.txt"
+	    			         + 'Content-Disposition: form-data; name="file";'
+	    			         + 'filename="' + file_name + '"\r\n'
+	    			         // Add the file's mime-type
+	    			         + 'Content-type: plain/text\r\n\r\n'
+	    			         // Add your data:
+	    			         + encrypted + '\r\n'
+	    			         + '--'+ boundary + '--';
 
+	    			$.ajax({
+	    			    contentType: "multipart/form-data; boundary="+boundary,
+	    			    data: body,
+	    			    type: "POST",
+	    			    url: "back.php",
+	    			    success: function (data, status) {
+	    			    	console.log(data,status);
+	    			    }
+	    			});
 	    		}
-	    		console.log(reader.readAsText(file));
+	    		console.log(reader.readAsDataURL(file));
+
 	    		// var decrypted = CryptoJS.AES.decrypt(encrypted, group_access_key).toString(CryptoJS.enc.Latin1); 
 	    		// console.log("cipherText: ",decrypted);
 	    	});
