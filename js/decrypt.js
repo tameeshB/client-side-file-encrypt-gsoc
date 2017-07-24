c506d9972b18cc61d7d299163933e188e041cf36
tameeshb <tameeshbiswas1998@gmail.com>
2017-07-24 09:08:12 +0530

    Image preview

--- js/decrypt.js
+++ js/decrypt.js
@@ -14,6 +14,43 @@
 	        pom.click();
 	    }
 	}
+	var BASE64_MARKER = ';base64,';
+
+	function convertDataURIToBinary(dataURI) {
+	  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+	  var base64 = dataURI.substring(base64Index);
+	  var raw = window.atob(base64);
+	  var rawLength = raw.length;
+	  var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+	  for(i = 0; i < rawLength; i++) {
+	    array[i] = raw.charCodeAt(i);
+	  }
+	  return array;
+	}
+	function dataURLtoFile(dataurl, filename) {
+	    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+	        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+	    while(n--){
+	        u8arr[n] = bstr.charCodeAt(n);
+	    }
+	    return new File([u8arr], filename, {type:mime});
+	}
+	function setBase64ToImage(baseStringIP){
+
+		var baseString = baseStringIP.trim();
+		// data:image/png;base64
+		
+		if(baseString.substring(0,4) != "data"){
+			baseString = "data:image/png;base64," + baseString;
+		}
+		
+		$("#previewImg").prop('src',baseString);	
+		$("#previewImg").addClass("span12 baseurlopa2");
+		$("#dwnldLink").show();
+		$("#dwnldLink").prop('href',baseString);
+	}
+
   // Jquery onload function.
   var file = null;
   $(document).ready(function(){
@@ -41,7 +78,10 @@
 	    			var decrypted = CryptoJS.AES.decrypt(event_.target.result, group_access_key).toString(CryptoJS.enc.Latin1); 
 	    			// var encrypted = CryptoJS.AES.encrypt(event_.target.result, group_access_key); 
 	    			console.log("clearText: ",decrypted);
-	    			download(file_name,decrypted);
+	    			// var file = dataURLtoFile(decrypted, file_name);
+	    			setBase64ToImage(decrypted);
+	    			// console.log(file);
+	    			// download(file_name,file);
 
 	    		}
 	    		console.log(reader.readAsText(file));
