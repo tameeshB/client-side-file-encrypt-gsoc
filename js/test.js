cfbe64bc0ecdc58ed1b41e624865ec1e27525ee0
tameeshb <tameeshbiswas1998@gmail.com>
2017-08-22 02:27:34 +0530

    Code Review fixes 1

--- js/test.js
+++
@@ -1,7 +0,0 @@
-// Jquery wrapper for drupal to avoid conflicts between libraries.
-(function ($) {
-  // Jquery onload function.
-  $(document).ready(function(){
-  	console.log('lol');
-  });
-})(jQuery); 
